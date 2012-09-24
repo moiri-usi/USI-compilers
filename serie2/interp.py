51e35be53ec69f7f48b75e0cecb9922f66d5f91f
Lothar Rubusch <lothar.rudolf.rubusch@usi.ch>
2012-09-25 00:32:34 +0200

    arithmetic tests and nodes

--- serie2/interp.py
+++ serie2/interp.py
@@ -1,53 +1,170 @@
 # get file path from param
 import sys
-if len( sys.argv[1:] ) != 1:
-    print "ERROR, usage:\n" + sys.argv[0] + " <filename>"
-    print "e.g.\n" + sys.argv[0] + " ./input.txt"
-    sys.exit( 1 )
-
-path = sys.argv[1]
-
 import os.path
-if not os.path.exists( path ):
-    print "ERROR, file '%s' does not exist" % path
-    sys.exit( 1 )
-
-# parse file and generate ast
 import compiler
-ast = compiler.parseFile(path)
 
-# function to interprete the ast
-# @param obj n: node of the ast
-def num_nodes(n):
-    if isinstance(n, compiler.ast.Module):
-        return num_nodes(n.node)
+"""
+USAGE:
 
-    elif isinstance(n, compiler.ast.Stmt):
-        return 1 + sum([num_nodes(x) for x in n.nodes])
+python ./interp.py ./input.txt
 
-    elif isinstance(n, compiler.ast.Add):
-        return 1 + num_nodes(n.left) + num_nodes(n.right)
-   
-    elif isinstance(n, compiler.ast.Const):
-        return n.value # declare as const by python?
+TEST
+python ./test_interp.py
+"""
 
-    elif isinstance(n, compiler.ast.Discard):
-        num_nodes(n)
-        return
-    #Mul
-    #Sub
-    #Div
-    #AssName
-    #Bitand
-    #Bitor
-    #Bitxor
-    #CallFunc
-    #List
-    #Name
-    #Println
-    #UnarySub
-    #UnaryAdd
-    else:
-        print "unknown ast node"
-# interprete ast
-num_nodes(ast)
+
+def die( meng ):
+    print meng
+    sys.exit( -1 )
+
+class Interpreterer( object ):
+    def __init__( self, filepath=None ):
+        if filepath:
+            if not os.path.exists( filepath ):
+                die( "ERROR: file '%s' does not exist" % filepath )
+            else:
+                self.ast = compiler.parseFile( filepath )
+        self.stack = []
+        self.vartable = {}
+
+    def interpret_file( self ):
+        try:
+            return self.num_nodes( self.ast )
+        except AttributeError:
+            die( "ERROR: class started in DEBUG mode, no AST file set" )
+
+    def stack_push( self, elem):
+        self.stack.append( elem )
+
+    def stack_pop( self ):
+        return self.stack.pop()
+
+    def stack_peek( self ):
+        try:
+            return self.stack[0]
+        except IndexError:
+            # TODO check raise
+            raise IndexError( "ERROR: stack index error" )
+
+    def vartable_set( self, name, val ):
+        self.vartable.update( {name:val} )
+
+    def vartable_get( self, name ):
+        return self.vartable[key]
+
+    def num_child_nodes( self, node ):
+        num = sum([self.num_nodes(x) for x in node.getChildNodes()])
+        return num
+
+    ## function to interprete the ast
+    ## @param obj n: node of the ast
+    def num_nodes(self, node):
+        if isinstance( node, compiler.ast.Module):
+            print "\tModule" 
+            # flush stack
+            self.stack = []
+            # flush vartable
+            self.vartable = {}
+            return self.num_nodes(node.node)
+
+        elif isinstance( node, compiler.ast.Stmt):
+            print "\t\tStmt" 
+            ## loop over all nodes, thereby execute node operation, construct
+            ## list of nodes to count num of nodes as return value of "num_nodes()"
+            try:
+                num = 1 + self.num_child_nodes( node )
+                return num
+            except:
+                print "ERROR: None Type received, one node returned NOTHING"
+                return -1
+
+        elif isinstance(node, compiler.ast.Add):
+            print "\t\tAdd" 
+            num = 1 + self.num_child_nodes( node )
+            self.stack_push( self.stack_pop() + self.stack_pop() )
+            return num
+
+        elif isinstance(node, compiler.ast.Mul ):
+            print "\t\tMul" 
+            num = 1 + self.num_child_nodes( node )
+            self.stack_push( self.stack_pop() * self.stack_pop() )
+            return num
+
+        elif isinstance(node, compiler.ast.Sub ):
+            print "\t\tSub" 
+            num = 1 + self.num_child_nodes( node )
+            var_a = self.stack_pop()
+            var_b = self.stack_pop()
+            self.stack_push( var_b - var_a )
+            return num
+
+        elif isinstance(node, compiler.ast.Div ):
+            print "\t\tDiv" 
+            num = 1 + self.num_child_nodes( node )
+            var_a = self.stack_pop()
+            var_b = self.stack_pop()
+#            print "XXX num '%d', var_a '%d', var_b '%d'" % (num, var_a, var_b)   
+            try: # FIXME, exit on ZeroDivisionError
+                self.stack_push( var_b / var_a )
+            except ZeroDivisionError:
+                die( "ERROR: division by 0" )
+            return num
+
+        elif isinstance(node, compiler.ast.Const):
+            print "\t\tConst" 
+            ## TODO const flag, no assingment to const
+            if type( node.value ) is not int:
+                print "WARNING: casting float to integer"  
+                node.value = int( node.value )
+            self.stack_push( node.value )
+            return 1
+
+        elif isinstance(node, compiler.ast.Discard):
+            print "\t\tDiscard" 
+            return 1 + self.num_child_nodes( node )
+
+        elif isinstance(node, compiler.ast.AssName ):
+            return 1    
+
+        elif isinstance(node, compiler.ast.Assign ):
+            return 1    
+
+        elif isinstance(node, compiler.ast.Bitand ):
+            return 1    
+
+        elif isinstance(node, compiler.ast.Bitor ):
+            return 1    
+
+        elif isinstance(node, compiler.ast.Bitxor ):
+            return 1    
+
+        elif isinstance(node, compiler.ast.CallFunc ):
+            return 1    
+
+        elif isinstance(node, compiler.ast.List ):
+            return 1    
+
+        elif isinstance(node, compiler.ast.Name ):
+            return 1    
+
+        elif isinstance(node, compiler.ast.Printnl ):
+            print "\t\tPrintnl"
+            num = 1 + self.num_child_nodes( node )
+            print "%s" % str( self.stack_pop() )
+            return num
+
+        elif isinstance(node, compiler.ast.UnarySub ):
+            return 1    
+
+        elif isinstance(node, compiler.ast.UnaryAdd ):
+            return 1    
+
+        else:
+            print "unknown ast node"
+            return 1
+
+## start
+if 1 == len( sys.argv[1:] ):
+    inst = Interpreterer( sys.argv[1] )
+    number_of_nodes = inst.interpret_file()
+
--- serie2/test_interp.py
+++ serie2/test_interp.py
@@ -1,61 +1,101 @@
-import random
+import compiler
 import unittest
+from interp import *
+#from interp import Interpreterer
+
 
 """
 USAGE
 
-python ./unittest_demo.py
-
-
-RESOURCES
-
-unittest:
-http://docs.python.org/library/unittest.html
-
-asserts:
-http://docs.python.org/library/unittest.html#assert-methods
-
+python ./test_interp.py
 """
 
 class TestSequenceFunctions( unittest.TestCase ):
 
-## When a setUp() method is defined, the test runner will run that method prior
-## to each test.
     def setUp(self):
-        self.seq = range(10)
+         self.intp = Interpreterer()
 
 ## Likewise, if a tearDown() method is defined, the test runner will invoke that
 ## method after each test.
-#    def tearDown(self):
-#        # TODO
+    # def tearDown(self):
+    #     # TODO
 
-## several test cases implemented with exceptions
-##
-## The three individual tests are defined with methods whose names start with
-## the letters test.
-## The crux of each test is a call to assertEqual() to check for an expected
-## result; assertTrue() to verify a condition; or assertRaises() to verify that
-## an expected exception gets raised. These methods are used instead of the
-## assert statement so the test runner can accumulate all test results and
-## produce a report.
-    def test_shuffle(self):
-        # make sure the shuffled sequence does not lose any elements
-        random.shuffle(self.seq)
-        self.seq.sort()
-        self.assertEqual(self.seq, range(10))
+    def test_Add( self ):
+        ast = compiler.parse( "1+2" )
+        res = self.intp.num_nodes( ast )
 
-        # should raise an exception for an immutable sequence
-        self.assertRaises(TypeError, random.shuffle, (1,2,3))
+        ## test result
+        self.assertEqual( self.intp.stack[0], 3 )
 
-    def test_choice(self):
-        element = random.choice(self.seq)
-        self.assertTrue(element in self.seq)
+        ## test number of executed nodes
+        self.assertEqual( 5, res )   
 
-    def test_sample(self):
-        with self.assertRaises(ValueError):
-            random.sample(self.seq, 20)
-        for element in random.sample(self.seq, 5):
-            self.assertTrue(element in self.seq)
+    def test_Sub( self ):
+        ast = compiler.parse( "2-1" )
+        res = self.intp.num_nodes( ast )
+
+        ## test result
+        self.assertEqual( self.intp.stack_peek(), 1 )
+
+        ## test number of executed nodes
+        self.assertEqual( 5, res )   
+
+    def test_Sub__negativevalue( self ):
+        ast = compiler.parse( "1-2" )
+        res = self.intp.num_nodes( ast )
+
+        ## test result
+        self.assertEqual( self.intp.stack_peek(), -1 )
+
+        ## test number of executed nodes
+        self.assertEqual( 5, res )   
+
+    def test_Mul( self ):
+        ast = compiler.parse( "2*10" )
+        res = self.intp.num_nodes( ast )
+
+        ## test result
+        self.assertEqual( self.intp.stack_peek(), 20 )
+
+        ## test number of executed nodes
+        self.assertEqual( 5, res )   
+
+    def test_Div( self ):
+        ast = compiler.parse( "10/5" ) # TODO test division by zero
+        res = self.intp.num_nodes( ast )
+
+        ## test result
+        self.assertEqual( self.intp.stack_peek(), 2 )
+
+        ## test number of executed nodes
+        self.assertEqual( 5, res )   
+
+
+
+
+
+
+# TODO rm
+    # def test_shuffle(self):
+    #     # make sure the shuffled sequence does not lose any elements
+    #     random.shuffle(self.seq)
+    #     self.seq.sort()
+    #     self.assertEqual(self.seq, range(10))
+
+    #     # should raise an exception for an immutable sequence
+    #     self.assertRaises(TypeError, random.shuffle, (1,2,3))
+
+    # def test_choice(self):
+    #     element = random.choice(self.seq)
+    #     self.assertTrue(element in self.seq)
+
+    # def test_sample(self):
+    #     with self.assertRaises(ValueError):
+    #         random.sample(self.seq, 20)
+    #     for element in random.sample(self.seq, 5):
+    #         self.assertTrue(element in self.seq)
+
+
 
 ## start
 
@@ -68,12 +108,5 @@ class TestSequenceFunctions( unittest.TestCase ):
 ## The final block shows a simple way to run the tests. unittest.main() provides
 ## a command-line interface to the test script. When run from the command line,
 ## the above script produces an output that looks like this
-suite = unittest.TestLoader().loadTestsFromTestCase(TestSequenceFunctions)
-unittest.TextTestRunner(verbosity=2).run(suite)
-
-
-
-
-## for commandline, run
-# python -m unittest -h
-
+suite = unittest.TestLoader().loadTestsFromTestCase( TestSequenceFunctions )
+unittest.TextTestRunner( verbosity=2 ).run( suite )
