ba6762524acf5c8c0ca151b024fc792d23410a56
Simon Maurer <moirelein@gmail.com>
2012-11-10 00:17:18 +0100

    add simple script to generate ig images

---
+++ compilerP0/ig.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+##
+## usage: ./ig.sh FILE
+
+
+mkdir graph
+./compile.py -ig $1 > graph/ig.gv
+./compile.py -ig-color $1 > graph/ig-color.gv
+dot -Tpng graph/ig.gv -o graph/ig.png
+dot -Tpng graph/ig-color.gv -o graph/ig-color.png
+
+gthumb graph/ig.png
+rm -r graph
