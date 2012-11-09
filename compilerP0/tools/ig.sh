599730ac9e1f8f67ab531e59ec676e9d04cd2a62
Simon Maurer <moirelein@gmail.com>
2012-11-10 00:55:02 +0100

    set default input file

--- compilerP0/tools/ig.sh
+++ compilerP0/tools/ig.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 ##
 ## usage: ./ig.sh FILE
-
+path=$1
+if [[ $path == "" ]]; then
+    path="../input/input.p0"
+fi
 mkdir graph
-./../compile.py -ig $1 > graph/ig.gv
-./../compile.py -ig-color $1 > graph/ig-color.gv
+./../compile.py -ig $path > graph/ig.gv
+./../compile.py -ig-color $path > graph/ig-color.gv
 dot -Tpng graph/ig.gv -o graph/ig.png
 dot -Tpng graph/ig-color.gv -o graph/ig-color.png
 
