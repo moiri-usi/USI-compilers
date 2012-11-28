2d38d81a11b4dfa5edd04d921852a9208edb50ba
jotroick <jotroick@gmail.com>
2012-11-28 15:41:24 +0100

    TEST CASES P2.5

---
+++ TestCasesP2.5/t1_Boolean.py
@@ -0,0 +1,17 @@
+print True
+
+print False
+
+print True + 2
+
+print False - True
+
+print False - False + True + False - True
+
+print True + 1.1 + 12 + False * True
+
+print True / True
+
+print True * 0.256
+
+## In Python, false can also be written as 0 and true as 1.
---
+++ TestCasesP2.5/t3_Compar_logical.py
@@ -0,0 +1,15 @@
+'a' == ('a' or 'b')
+'b' == ('a' or 'b')
+'a' == ('a' and 'b')
+'b' == ('a' and 'b')
+
+"""
+>>> 'a' == ('a' or 'b')
+True
+>>> 'b' == ('a' or 'b')
+False
+>>> 'a' == ('a' and 'b')
+False 
+>>> 'b' == ('a' and 'b')
+True
+"""
